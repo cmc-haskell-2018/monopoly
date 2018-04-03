a5a07b9a09131f273f9d2fb796d2160e46b4e740
Nadya <nadynady98@mail.ru>
2018-04-03 14:54:01 +0300

    land -> Model.hs

--- src/Model.hs
+++ src/Model.hs
@@ -54,3 +54,287 @@ data Cubes = Cubes
   , secondCube :: Int
   }
 
+getLand :: [Street]
+getLand =
+    [ Street
+      { name ="Старт"
+      , price = 0 
+      , isRent = True     -- Для специальных полей, которые нельзя купить - всегда True
+      , priceRent = 0
+      , owner = 4         -- Для специальных  полей - фиктивный пятый игрок
+      }
+    , Street
+      { name = "СКИ Квантовая информатика"
+      , price = 60
+      , priceRent = 6
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "Общественная казна"
+      , price = 0
+      , isRent = True
+      , priceRent = 0
+      , owner = 4
+      }
+    , Street 
+      { name = "СКИ Параллельные вычисления"
+      , price = 60
+      , priceRent = 7
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "Налог" -- Смотреть описание для "Сверхналога"
+      , priceRent = 200
+      , price = 0
+      , owner = 4
+      , isRent = True
+      }
+    , Street 
+      { name = "Машзал 1"
+      , price = 200
+      , priceRent = 20
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "СП ПЦД"
+      , price = 100
+      , priceRent = 12
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "Шанс"
+      , price = 0
+      , isRent = True
+      , priceRent = 0
+      , owner = 4
+      }
+    , Street 
+      { name = "СП УДИС"
+      , price = 100
+      , priceRent = 10
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "СП Корректность программ"
+      , price = 120
+      , priceRent = 14
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "Академ"
+      , price = 0
+      , isRent = True
+      , priceRent = 0
+      , owner = 4
+      }
+    , Street
+      { name = "МатКиб ДММК"
+      , price = 140
+      , priceRent = 15
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "Потеряшки"
+      , price = 150
+      , priceRent = 15
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "МатКиб ДФСА"
+      , price = 140
+      , priceRent = 14
+      , isRent = False
+      , owner = 0
+      }
+    , Street
+      { name = "Маткиб Дискретный анализ"
+      , price = 160
+      , priceRent = 18
+      , isRent = False
+      , owner = 0
+      }
+    , Street
+      { name = "Машзал 2"
+      , price = 200
+      , priceRent = 25
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "ИО Морозов"
+      , price = 180
+      , priceRent = 20
+      , isRent = False
+      , owner = 0
+      }
+    , Street
+      { name = "Общественная казна"
+      , price = 0
+      , isRent = True
+      , priceRent = 0
+      , owner = 4
+      }
+    , Street
+      { name = "ИО Новикова"
+      , price = 180
+      , priceRent = 19
+      , isRent = False
+      , owner = 0
+      }
+    , Street
+      { name = "ИО Денисов"
+      , price = 200
+      , priceRent = 23
+      , isRent = False
+      , owner = 0
+      }
+    , Street
+      { name = "Бесплатная курилка"
+      , price = 0
+      , isRent = True
+      , priceRent = 0
+      , owner = 4
+      }
+    , Street
+      { name = "МАТСТАТ Теория рисков"
+      , price = 220
+      , priceRent = 22
+      , isRent = False
+      , owner = 0
+      }
+    , Street
+      { name = "Шанс"
+      , price = 0
+      , isRent = True
+      , priceRent = 0
+      , owner = 4
+      }
+    , Street
+      { name = "МАТСТАТ ДГМС"
+      , price = 220
+      , priceRent = 24
+      , isRent = False
+      , owner = 0
+      }
+    , Street
+      { name = "МАТСТАТ МОТВЫ"
+      , price = 240
+      , priceRent = 25
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "Машзал 3"
+      , price = 200
+      , priceRent = 22
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "АСВК ЛБИС"
+      , price = 260
+      , priceRent = 28
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "АСВК ЛВК"
+      , price = 260
+      , priceRent = 29
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "Техносфера"
+      , price = 150
+      , priceRent = 16
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "АСВК Медиалаб"
+      , price = 280
+      , priceRent = 30
+      , isRent = False
+      , owner = 0
+      }
+    , Street 
+      { name = "Отправляйся в академ"
+      , price = 0
+      , isRent = True
+      , priceRent = 0
+      , owner = 4
+      }
+    , Street 
+      { name = "АЯ Парадигмы программирования"
+      , price = 300
+      , priceRent = 35
+      , isRent = False
+      , owner = 0
+      }
+    , Street
+      { name = "АЯ Компьютерная лингвистика"
+      , price = 300
+      , priceRent = 35
+      , isRent = False
+      , owner = 0
+      }
+    , Street
+      { name = "Общественая казна"
+      , price = 0
+      , isRent = True
+      , priceRent = 0
+      , owner = 4
+      }
+    , Street
+      { name = "АЯ Искусственный интеллект"
+      , price = 320
+      , priceRent = 32
+      , isRent = False
+      , owner = 0
+      }
+    , Street
+      { name = "Машзал 4"
+      , price = 200
+      , priceRent = 21
+      , isRent = False
+      , owner = 0
+      }
+    , Street
+      { name = "Шанс"
+      , price = 0
+      , isRent = True
+      , priceRent = 0
+      , owner = 4
+      }
+    , Street
+      { name = "ММП МАТ"
+      , price = 350
+      , priceRent = 37
+      , isRent = False
+      , owner = 0
+      }
+    , Street
+      { name = "Сверхналог"
+      , priceRent = 100
+      , price = 0
+      , owner = 4 -- По алгоритму работы никто не сможет купить, потому что isRent = True
+      , isRent = True -- Но при этом все будут платить мнимому 5ому игроку 100$
+      }
+    , Street
+      { name = "ММП БММО"
+      , price = 400
+      , priceRent = 40
+      , isRent = False
+      , owner = 0
+      }
+    ]
+
